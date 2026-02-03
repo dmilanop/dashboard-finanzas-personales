ae6562df6b9dbc5d383773164cf0dbebcd3fce7c
DiegoDev <dmilanop28@gmail.com>
2026-02-02 23:32:14 -0400

    :wrench: feat: creando ruta post

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const connectDB = require('./config/db');
+const User = require('./models/UserSchema');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,9 +10,27 @@ const PORT = process.env.PORT || 4000;
 connectDB();
 app.use(express.json());
 
+// Ruta GET de prueba
 app.get('/', (req, res) => {
     res.send('Primera prueba de servidor backend con nodemon');
 });
+
+// Ruta POST para crear un nuevo usuario 
+app.post('/api/users', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        res.status(201).json(newUser);
+
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "No se pudo guardar el producto",
+            error: error.message
+        });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor en puerto ${PORT}`)
