c7d75595982449bcefa88d388fd745ec8f79cb2c
DiegoDev <dmilanop28@gmail.com>
2026-02-02 23:31:13 -0400

    :wrench: feat: prueba de creación de usuario

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// const User = require('../models/UserSchema');
 
 // main().catch(err => console.log(err));
 
@@ -12,6 +13,18 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('✅ MongoDB conectado exitosamente');
+
+        // Prueba de creación de un usuario
+        
+        // const newUser = new User({
+        //     id: 1,
+        //     name: 'Diego',
+        //     lastname: 'Perez'
+        // });
+
+        // await newUser.save();
+        // console.log("✅ Usuario de prueba guardado con éxito");
+
     } catch (error) {
         console.error('❌ Error de conexión:', error.message);
         process.exit(1)
