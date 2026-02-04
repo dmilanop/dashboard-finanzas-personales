6b6e837d4294f17f7ac48e0eea0f3ce6a19187e0
DiegoDev <dmilanop28@gmail.com>
2026-02-03 22:14:50 -0400

    :wrench: feat: creando ruta POST para una nueva transacción

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const User = require('./models/UserSchema');
+const Transaction = require('./models/TransactionSchema');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -30,7 +31,23 @@ app.post('/api/users', async (req, res) => {
             error: error.message
         });
     }
-})
+});
+
+// Ruta POST para crear una nueva transacción
+app.post('/api/transactions', async (req, res) => {
+    try {
+        const newTransaction = new Transaction(req.body);
+        await newTransaction.save();
+
+        res.status(201).json(newTransaction);
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "No se pudo guardar la transacción",
+            error: error.message
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor en puerto ${PORT}`)
