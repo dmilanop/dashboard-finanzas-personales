70b2a85d580c08baac419a1f527e5b07f5c0fc72
DiegoDev <dmilanop28@gmail.com>
2026-02-04 22:12:30 -0400

    :hammer_and_wrench: refactor: modificando el schema y añadiendo los tipos

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    id: Number,
-    name: String,
-    lastname: String,
+    name: { type: String, required: true},
+    lastname: { type: String, required: true},
 });
 
 module.exports = mongoose.model('User', UserSchema);
