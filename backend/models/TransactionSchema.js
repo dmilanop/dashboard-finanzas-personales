6e2ae7f1947c10dbe622314da2b69addf458b62c
DiegoDev <dmilanop28@gmail.com>
2026-02-04 22:12:53 -0400

    :hammer_and_wrench: refactor: modificando el schema y añadiendo los tipos

--- backend/models/TransactionSchema.js
+++ backend/models/TransactionSchema.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TransactionSchema = new Schema({
-    description: String,
-    userId: Number,
-    amount: Number,
-    currency: String,
-    type: String,
-    category: String,
+    description: { type: String, required: true },
+    amount: { type: Number, required: true },
+    currency: { type: String, required: true, enum: ['USD', 'EUR', 'BS', 'USDT'] },
+    type: { type: String, required: true, enum: ['Ingreso', 'Egreso'] },
+    category: { type: String, required: true, enum: ['Sueldo', 'Comida', 'Transporte', 'Salud', 'Entretenimiento', 'Otros'] },
     date: { type: Date, default: Date.now},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
