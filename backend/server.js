27f4aea1cc36fe4835e4c0dd0f20981d5ce47a00
DiegoDev <dmilanop28@gmail.com>
2026-02-05 22:26:35 -0400

    :hammer_and_wrench: refactor: modificando la invocación de las rutas

--- backend/server.js
+++ backend/server.js
@@ -2,52 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const connectDB = require('./config/db');
-const User = require('./models/UserSchema');
-const Transaction = require('./models/TransactionSchema');
-
+const user = require('./routes/user');
+const transaction = require('./routes/transaction');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Conección a la base de datos
 connectDB();
+
+// Middleware
 app.use(express.json());
 
-// Ruta GET de prueba
-app.get('/', (req, res) => {
-    res.send('Primera prueba de servidor backend con nodemon');
-});
+// Ruta Users
+app.use('/api/users', user);
 
-// Ruta POST para crear un nuevo usuario 
-app.post('/api/users', async (req, res) => {
-    try {
-        const newUser = new User(req.body);
-        await newUser.save();
-
-        res.status(201).json(newUser);
-
-    } catch (error) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "No se pudo guardar el producto",
-            error: error.message
-        });
-    }
-});
-
-// Ruta POST para crear una nueva transacción
-app.post('/api/transactions', async (req, res) => {
-    try {
-        const newTransaction = new Transaction(req.body);
-        await newTransaction.save();
-
-        res.status(201).json(newTransaction);
-    } catch (error) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "No se pudo guardar la transacción",
-            error: error.message
-        });
-    }
-});
+// Ruta Transactions
+app.use('/api/transactions', transaction);
 
 app.listen(PORT, () => {
     console.log(`🚀 Servidor en puerto ${PORT}`)
