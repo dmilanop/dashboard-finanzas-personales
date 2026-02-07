d1f16bd0057206feec32bc243683724fc8778eaf
DiegoDev <dmilanop28@gmail.com>
2026-02-06 21:53:43 -0400

    :hammer_and_wrench: refactor: añadiendo los contorladores a cada una de las peticiones

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,42 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/UserSchema');
+const getUsers = require('../controllers/userController').getUsers;
+const createUser = require('../controllers/userController').createUser;
 
 // Obtener todos los usuarios
-router.get('/', async (req, res) => {
-    try {
-        const users = await User.find();
-
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            message: "Error al obtener los usuarios",
-            error: error.message
-        });
-    };
-});
+router.get('/', getUsers);
 
 // Crear un nuevo usuario
-router.post('/signup', async (req, res) => {
-    const newUser = new User(req.body);
-
-    try {
-        const savedUser = await newUser.save();
-
-        res.status(201).json({
-            ok: true,
-            mensaje: "Usuario creado exitosamente",
-            user: savedUser
-        });
-
-    } catch (error) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "No se pudo crear el usuario, llenar todos los campos",
-            error: error.message
-        });
-    };
-});
+router.post('/signup', createUser);
 
 module.exports = router;
