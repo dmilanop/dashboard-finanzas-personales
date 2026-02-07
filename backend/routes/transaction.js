c97a6d774becf06ab21bdd42868ad61e02a5278f
DiegoDev <dmilanop28@gmail.com>
2026-02-06 21:54:09 -0400

    :hammer_and_wrench: refactor: añadiendo los contorladores a cada una de las peticiones

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -1,47 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Transaction = require('../models/TransactionSchema');
+const getTransactions = require('../controllers/transactionController').getTransactions;
+const createTransaction = require('../controllers/transactionController').createTransaction;
 
 // Obtener todas las transacciones
-router.get('/', async (req, res) => {
-    try {
-        const transactions = await Transaction.find();
-
-        res.status(200).json({
-            ok: true,
-            message: "Transacciones obtenidas exitosamente",
-            transactions
-        });
-
-    } catch (error) {
-        res.status(500).json({
-            ok: false,
-            message: "Fallo al obtener las transacciones, error del servidor",
-            error: error.message
-        });
-    };
-});
+router.get('/', getTransactions);
 
 // Crear una nueva transacción
-router.post('/new-transaction', async (req, res) => {
-    const newTransaction = new Transaction(req.body);
-
-    try {
-        const savedTransaction = await newTransaction.save();
-
-        res.status(201).json({
-            ok: true,
-            message: "Transacción creada existosamente",
-            transaction: savedTransaction
-        });
-
-    } catch (error) {
-        res.status(400).json({
-            ok: false,
-            message: "No se pudo crear la transacción, llenar todos los campos",
-            error: error.message
-        });
-    };
-});
+router.post('/new-transaction', createTransaction);
 
 module.exports = router;
