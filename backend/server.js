9d1474de71981bd72c0dc382a26b8673c1d9c0a8
DiegoDev <dmilanop28@gmail.com>
2026-02-09 20:34:22 -0400

    :wrench: feat: añadiendo y probando middelwares

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const user = require('./routes/user');
 const transaction = require('./routes/transaction');
+const myLogger = require('./middlewares/myLoggerMiddleware');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -12,9 +13,11 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+app.use(myLogger);
 
 // Ruta Users
 app.use('/api/users', user);
+
 
 // Ruta Transactions
 app.use('/api/transactions', transaction);
