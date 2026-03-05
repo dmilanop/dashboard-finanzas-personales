6705ead91b5734670042f82ec1212eeffcfedadb
DiegoDev <dmilanop28@gmail.com>
2026-03-05 17:15:06 -0400

    :wrench: feat: nuevos campos en el usuario

--- backend/validators/userValidator.js
+++ backend/validators/userValidator.js
@@ -1,8 +1,10 @@
 const { body } = require('express-validator');
 
 const userRules = [
+    body('email').isEmail().withMessage('El correo electrónico no es válido'),
     body('name').notEmpty().withMessage('El nombre es requerido'),
     body('lastname').notEmpty().withMessage('El apellido es requerido'),
+    body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
 ];
 
 module.exports = {
