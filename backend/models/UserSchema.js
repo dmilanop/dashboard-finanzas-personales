1e9be20d33544754454d2e41e08eab448ec0d065
DiegoDev <dmilanop28@gmail.com>
2026-03-11 18:58:15 -0400

    :wrench: feat: añadir encriptación de la contraseña

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -7,5 +8,21 @@ const UserSchema = new Schema({
     lastname: { type: String, required: true},
     password: { type: String, required: true },
 });
+
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+
+        this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+        throw error;
+    }
+});
+
+UserSchema.methods.comparePassword = async function (userPassword) {
+    return await bcrypt.compare(userPassword, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
